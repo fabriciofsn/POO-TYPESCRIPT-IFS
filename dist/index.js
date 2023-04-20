2772f1a1aeca6ee01a8d1cef923c55b42c5c4c21
Fabricio souza <banksapk@gmail.com>
2023-04-20 20:33:02 -0300

    aula

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,33 @@
 "use strict";
-let primeiro;
-let sobrenome;
-let nomeCompleto;
-let idade;
-let brasileiro;
-let enderecos;
-primeiro = "will";
-sobrenome = "smith";
-idade = 43;
-brasileiro = false;
-nomeCompleto = `${primeiro} ${sobrenome}`;
-enderecos = ["Rua B, Aracaju", "Rua C, Tobias Barreto"];
-let exibirNomeCompleto = () => {
-    return `Nome completo: ${nomeCompleto}`;
-};
-let recuperarEnderecos = (posicao) => {
-    return enderecos[posicao];
-};
-console.log(exibirNomeCompleto());
-console.log(recuperarEnderecos(1));
+class Pessoa {
+    constructor(nome, sobrenome, idade, brasileiro, endereco) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.brasileiro = brasileiro;
+        this.endereco = endereco;
+    }
+    nomeCompleto() {
+        return `Nome completo: ${this.nome} ${this.sobrenome}`;
+    }
+    recuperarEndereco(posicao) {
+        return `Endereço: ${this.endereco[posicao]}`;
+    }
+    nacionalidade() {
+        let br = "Brasileiro";
+        if (this.brasileiro) {
+            return `${br}? Sim`;
+        }
+        else {
+            return `${br}? Não`;
+        }
+    }
+    getIdade() {
+        return `Idade: ${this.idade}`;
+    }
+}
+const pessoa = new Pessoa("Caça", "Rato", 25, true, ["Rua A", "Rua B"]);
+console.log(pessoa.nomeCompleto());
+console.log(pessoa.nacionalidade());
+console.log(pessoa.getIdade());
+console.log(pessoa.recuperarEndereco(1));
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
-let primeiro: string;
-let sobrenome: string;
-let nomeCompleto: string;
-let idade: number;
-let brasileiro: boolean;
-let enderecos: Array<string>;
+class Pessoa {
+  private nome: string;
+  private sobrenome: string;
+  private idade: number;
+  private brasileiro: boolean;
+  private endereco: Array<string>;
 
-primeiro = "will";
-sobrenome = "dos Santos";
-idade = 43;
-brasileiro = false;
-nomeCompleto = `${primeiro} ${sobrenome}`;
-enderecos = ["Rua B, Aracaju", "Rua C, Tobias Barreto"];
+  constructor(
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    brasileiro: boolean,
+    endereco: Array<string>
+  ) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+    this.brasileiro = brasileiro;
+    this.endereco = endereco;
+  }
 
-let exibirNomeCompleto = (): string => {
-  return `Nome completo: ${nomeCompleto}`;
-};
+  public nomeCompleto(): string {
+    return `Nome completo: ${this.nome} ${this.sobrenome}`;
+  }
 
-let recuperarEnderecos = (posicao: number): string => {
-  return enderecos[posicao];
-};
+  public recuperarEndereco(posicao: number): string {
+    return `Endereço: ${this.endereco[posicao]}`;
+  }
 
-console.log(exibirNomeCompleto());
-console.log(recuperarEnderecos(1));
+  public nacionalidade(): string {
+    let br: string = "Brasileiro";
+    if (this.brasileiro) {
+      return `${br}? Sim`;
+    } else {
+      return `${br}? Não`;
+    }
+  }
+
+  public getIdade(): string {
+    return `Idade: ${this.idade}`;
+  }
+}
+
+const pessoa: Pessoa = new Pessoa("Caça", "Rato", 25, true, ["Rua A", "Rua B"]);
+
+console.log(pessoa.nomeCompleto());
+console.log(pessoa.nacionalidade());
+console.log(pessoa.getIdade());
+console.log(pessoa.recuperarEndereco(1));
